de33ec938de205b6b8fc87d0aa1d8b6729b6dd96
Eugene Sokolov <eugine_s@ukr.net>
2013-05-30 17:20:27 +0300

    Updated Streams Example.

--- src/com/sysiq/java8/streams/StreamTest.java
+++ src/com/sysiq/java8/streams/StreamTest.java
@@ -2,32 +2,71 @@ package com.sysiq.java8.streams;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.IntBinaryOperator;
+import java.util.function.IntUnaryOperator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
 
 public class StreamTest {
 
-    public static void echoInteger(Integer i) {
-        System.out.println("echoInteger: " + i);
+    @Test
+    public void testLegacy() {
+        List<Integer> list = Arrays.asList(5, 1, 6, 1, 2, 3, 4);
+
+        List<Integer> result = new ArrayList<>();
+        for (Integer item: list) {
+            if (item > 3) {
+                result.add(item);
+            }
+        }
+
+        Collections.sort(result);
+
+        for (Integer item: result) {
+            System.out.println(item);
+        }
+
     }
 
     @Test
     public void testStream() {
-        Consumer<Integer> echoInt = StreamTest::echoInteger;
-
-        List<Integer> list = Arrays.asList(4, 5, 6, 1, 2, 3);
+        List<Integer> list = Arrays.asList(5, 1, 6, 1, 2, 3, 4);
         list.stream()
                 .filter((x) -> x > 3)
                 .sorted(Integer::compare)
-                .forEach(echoInt);
+                .forEach(System.out::println);
+    }
 
-        System.out.println("=====================");
-        list.forEach(echoInt);
-        list.sort(Integer::compare);
+    @Test
+    public void testMoreStream() {
+        //sum
+        int sum = IntStream.range(0, 5)
+                .reduce((x, y) -> x + y)
+                .getAsInt();
+        System.out.println("Sum: " + sum);
 
-        System.out.println("=====================");
-        list.forEach(StreamTest::echoInteger);
+        // x^2
+        System.out.println("==== x^2: ");
+        IntStream.range(0, 5)
+                .map(x -> x*x)
+                .forEach(System.out::println);
+
+        //distinct
+        System.out.println("==== Distinct: ");
+        Arrays.asList(1, 2, 1, 2, 3, 0, 1, 2, 1)
+                .stream()
+                .distinct()
+                .sorted(Integer::compareTo)
+                .limit(2)
+                .forEach(System.out::println);
+
     }
 
 
